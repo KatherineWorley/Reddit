37b65c68bb7a90341e8f8b5f70d7fcdc1e942f5e
Katherine Worley <kdesign216@yahoo.com>
2018-01-08 10:56:34 -0500

    add assocation between link and user

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,6 +1,6 @@
 class LinksController < ApplicationController
   before_action :set_link, only: [:show, :edit, :update, :destroy]
-  before_filter :authenticate_user!, except: [:index, :show] #Note before filter. Migh have issues with this.
+  before_action :authenticate_user!, except: [:index, :show] #Note before filter. Migh have issues with this.
   
 
   # GET /links
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :links
-  root to: "links#index"
+  root "links#index"
 end
